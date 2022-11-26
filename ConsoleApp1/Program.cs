7e6363db3b57a7224ec25d2ee00a414bb9456541
Oleg3es <112572678+Oleg3es@users.noreply.github.com>
2022-11-26 08:46:17 +0700

    project4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,11 @@
 ﻿int a = new int();
 Console.WriteLine("Type:\n\ta - insert value of a\n\tb - insert value of b\n\t+ - perform summation\n\t- - perform substraction\n\t* - perform multiplication\n\t/ - perform division");
 string command = Console.ReadLine();
+if (command != null)
+{
+
+
+
+
+
+};
